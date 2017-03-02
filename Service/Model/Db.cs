45763780683df02c9900b377bad8169e33904fd9
edward <780589391@qq.com>
2017-03-02 16:38:30 +0800

    修改Coding手册

--- Service/Model/Db.cs
+++ Service/Model/Db.cs
@@ -11,21 +11,6 @@ namespace Service.Model
         {
         }
 
-        /* EntityFramework CodeFirst 数据库迁移语法
-         * PM> Enable-Migrations -ContextTypeName [“绝对命名路径”即继承自DbContext的子类命名空间 + 该子类名] -MigrationsDirectory [生成Migration的文件夹名] -Force
-         * PM> Add-Migration [迁移文件的名字] -ConfigurationTypeName [生成Migration的文件夹下的Configuration类的“绝对命名路径”]
-         * PM> Update-Database -ConfigurationTypeName [生成Migration的文件夹下的Configuration类的“绝对命名路径”]
-         * 注意：只有单个DbContext时上面可不用带参数
-         */
-
-        /*使用Code First做ORM，一旦数据库表有变化，再运行时必须做Code First数据库迁移，否会会报“无法检查模型兼容性...”错误：
-         * PM> Enable-Migrations   启用Code First 迁移
-         * 成功之后会生成Migrations文件夹、Migrations文件夹下的Configuration.cs，可在Configuration.cs中的Seed方法添加测试数据，然后在执行以下语句
-         * PM> Add-Migration Initial  创建初始化迁移，Initial是随意命名的，用来命名创建好的迁移文件
-         * 成功后，创建了{DateStamp}_Initial.cs迁移文件，它包含了为数据库创建相关表的指令，之后再执行以下语句
-         * PM> Update-Database    更新数据库，如果要强行执行语句更新数据库则加个参数 -Force 即可
-         */
-
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             /*用Entity Framework的 Fluent API方式配置Code First 指定数据库对应的表名、主键，也可以用Data Annotation注解方式设置映射约定
